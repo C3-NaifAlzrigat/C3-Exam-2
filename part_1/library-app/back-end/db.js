529a55c81cd5ee985c1c5f4e9404afcf4418aadc
NaifAlzrigat <naif.alzrigat@gmail.com>
2021-09-05 19:56:51 +0300

    solve Q1 in part 1

--- part_1/library-app/back-end/db.js
+++ part_1/library-app/back-end/db.js
@@ -1,1 +1,11 @@
 // db
+
+const mongoose=require("mongoose");
+
+  db=mongoose.connect("mongodb://localhost:5000/books",{useNewUrlParser: true, useUnifiedTopology: true})
+.then(respnse=>
+    ()=>{console.log("db is connected")}
+    ,(err)=>console.log(err)
+    )
+
+    module.exports=db
--- part_1/library-app/back-end/server.js
+++ part_1/library-app/back-end/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+require("../back-end/db");
 const app = express();
 
 app.use(express.json())
